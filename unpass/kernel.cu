25fbac4458148b14c4da4d4e318cb55bd27da623
Aios-Ciao <thor_ichino@hotmail.co.jp>
2021-12-29 23:09:20 +0900

    update $31F9の総当たり計算と最後の14桁目が有効な文字コードになるかの判定
    GPU向きの構成に処理を大幅に修正

--- unpass/kernel.cu
+++ unpass/kernel.cu
@@ -177,6 +177,65 @@ __global__ void chrconvKernel(
 	}
 }
 
+__global__ void make_4col_fullpair(
+	unsigned char *pass4col,		// 格納先 自分の要素は求めたグローバルインデックス*4からはじまる4byte
+	unsigned char *chrmask,			// 有効文字コードかの判定用ビットマスク
+	const int  offset				// blockIdxのzに履かせる下駄
+)
+{
+	unsigned int	blocksize = blockDim.x * blockDim.y * blockDim.z;
+	unsigned int	thidx = (threadIdx.z * (blockDim.x * blockDim.y)) + (threadIdx.y * blockDim.x) + threadIdx.x;
+	unsigned int	gridx = ((gridDim.x * gridDim.y) * ((blockIdx.z + (offset*gridDim.z)))) + (gridDim.x * blockIdx.y) + blockIdx.x;
+	unsigned int	gblidx = gridx * blocksize + thidx;
+
+	unsigned char	col1 = blockIdx.z + (offset * gridDim.z);
+	unsigned char	col2 = blockIdx.y;
+	unsigned char	col3 = blockIdx.x * (blockDim.z * blockDim.y) + threadIdx.z * blockDim.y + threadIdx.y;
+	unsigned char	col4 = threadIdx.x;
+	
+	unsigned char unvalid;
+
+	unvalid = chrmask[col1] | chrmask[col2] | chrmask[col3] | chrmask[col4];
+
+	pass4col[gblidx] = unvalid | (col1 ^ col2 ^ col3 ^ col4);
+}
+
+// これまでのXOR結果と4桁ぶんのXORを求める
+__global__ void calcxor8(
+	unsigned char		*xor8,		// 演算結果
+	const unsigned char *xor4,		// これまでの演算結果
+	const unsigned int	idxG		// xor8のインデックス
+)
+{
+	unsigned int idxH = (blockIdx.x * 256) + threadIdx.x;
+
+	unsigned char W1 = xor4[idxG];
+	unsigned char W2 = xor4[idxH];
+	unsigned char enf = (W1 | W2) & 0x80;
+
+	xor8[idxH] = enf | (W1 ^ W2);
+}
+
+// 13桁目の導出とxor 0x07結果の妥当性判断
+__global__ void calcxor13_validchk(
+	unsigned char		*valid14,		// 14桁目が有効なら1を格納
+	const unsigned char *xor12,			// 12桁目まで導出されたXOR
+	const unsigned char *chrmask		// 有効な文字コードかどうか
+)
+{
+	unsigned char col13 = xor12[blockIdx.x] ^ threadIdx.x;	// 13桁目の導出 1677万要素の当該桁値
+
+	col13 |= chrmask[threadIdx.x];
+	col13 ^= 0x07;
+
+	valid14[(blockIdx.x*blockDim.x) + threadIdx.x] = !(col13 & 0x80);	// 無効な文字コードか判定をかける
+}
+
+// カーネル呼び出しテスト用
+__global__ void kerntest()
+{
+}
+
 
 using namespace std;
 
@@ -206,9 +265,29 @@ const unsigned char g_chrcode[256] =
 //const char _dict[] = { "ABCDEFGHIJKLMNOPQRSTUVWXYZ" };
 const char _dict[] = { "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!.-nmcﾅﾑｺ" };
 
+const unsigned char charvalidmask[64] = { 
+	0x00,0x00,0x00,0x00,0x00,0x00,0x80,0x80,	// 0x06と0x07が無効
+	0x00,0x00,0x00,0x00,0x00,0x00,0x80,0x80,
+	0x00,0x00,0x00,0x00,0x00,0x00,0x80,0x80,
+	0x00,0x00,0x00,0x00,0x00,0x00,0x80,0x80,
+	0x00,0x00,0x00,0x00,0x00,0x00,0x80,0x80,
+	0x00,0x00,0x00,0x00,0x00,0x00,0x80,0x80,
+	0x00,0x00,0x00,0x00,0x00,0x00,0x80,0x80,
+};
+
+cudaError_t chkthread(
+	unsigned char *cpu_result,
+	unsigned long	item_length
+);
+
+const unsigned long	WORKSIZE = 64 * 64 * 64 * 64;
+static unsigned char result[WORKSIZE];
 
 int main()
 {
+	chkthread(result, WORKSIZE);
+
+#if 0
 	unsigned char	pass_columns[PASSWORD_LEN_MAX] = { 0 };
 	int passed = -1;
 
@@ -266,10 +345,107 @@ int main()
     if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaDeviceReset failed!"); return 1; }
 
 	delete[] chkresult;
-
+#endif
 	return 0;
 }
 
+cudaError_t chkthread(
+	unsigned char *cpu_result,
+	unsigned long	item_length
+)
+{
+	cudaError_t cudaStatus;
+//	unsigned char *dev_result = 0;		// 判定結果
+	unsigned char *dev_xor_result = 0;		// 4桁ぶんのxor結果
+	unsigned char *dev_xor_result8 = 0;		// 8桁ぶんのxor結果(テンポラリ)
+	unsigned char *dev_xor_result12 = 0;	// 12桁ぶんのxor結果(テンポラリ)
+	unsigned char *dev_31F9_is07h = 0;		// 13桁目までxorを求めた結果に0x07でxorした値(14桁目の文字コード)が有効な文字コードか
+
+	unsigned char *dev_chrcode_mask = 0;	// 有効文字コード判定用マスク		有効は0x00、無効は0x80。
+
+	unsigned char *cpu_31F9_valid = 0;
+	unsigned long validcnt = 0;
+
+
+//	dim3	 block_530m(48, 8, 2), grid_530m(3, 48, 6);	// 文字48種 4桁分の並列計算グリッド用	 5.3M items
+	dim3	 block_1677m(64, 8, 2), grid_1677m(4, 64, 4);	// 文字64種 4桁分の並列計算グリッド用	16.8M items
+
+	cudaStatus = cudaSetDevice(0);																		if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?"); goto Error; }
+
+	// 4桁分総組合せのXORデータ生成
+	cudaStatus = cudaMalloc((void**)&dev_xor_result, 64 * 64 * 64 * 64 * sizeof(unsigned char));		if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMalloc failed!"); goto Error; }
+	cudaStatus = cudaMalloc((void**)&dev_xor_result8, 64 * 64 * 64 * 64 * sizeof(unsigned char));		if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMalloc failed!"); goto Error; }
+	cudaStatus = cudaMalloc((void**)&dev_xor_result12, 64 * 64 * 64 * 64 * sizeof(unsigned char));		if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMalloc failed!"); goto Error; }
+	cudaStatus = cudaMalloc((void**)&dev_31F9_is07h, 64 * 64 * 64 * 64 * 64 * sizeof(unsigned char));		if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMalloc failed!"); goto Error; }
+
+	// 64文字種分のコード有効無効情報
+	cudaStatus = cudaMalloc((void**)&dev_chrcode_mask, 64 * sizeof(unsigned char));						if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMalloc failed!"); goto Error; }
+	cudaStatus = cudaMemcpy(dev_chrcode_mask, charvalidmask, 64 * sizeof(unsigned char), cudaMemcpyHostToDevice);	if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy failed!"); goto Error; }
+
+	cudaStatus = cudaMallocHost((void**)&cpu_31F9_valid, 64 * 64 * 64 * 64 * 64 * sizeof(unsigned char));	if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMalloc failed!"); goto Error; }
+
+	// 4桁分総組合せのXORデータ計算
+	for (int offset = 0; offset < 16; ++offset) {
+		make_4col_fullpair <<<grid_1677m, block_1677m >>> (dev_xor_result, dev_chrcode_mask, offset);
+	}
+	cudaStatus = cudaGetLastError();			if (cudaStatus != cudaSuccess) { fprintf(stderr, "checkPassKernel launch failed: %s\n", cudaGetErrorString(cudaStatus)); goto Error; }
+	cudaStatus = cudaDeviceSynchronize();		if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus); goto Error; }
+
+	validcnt = 0;
+
+	// 8桁ぶんの組合せを計算する
+	for (int xor4idx = 0; xor4idx < (256 * 256 * 256); ++xor4idx) {
+
+		// xor4idx番目の組合せについて次の4桁を計算する
+		calcxor8 << < 256 * 256, 256 >> > (dev_xor_result8, dev_xor_result, xor4idx);
+		cudaStatus = cudaDeviceSynchronize();		if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus); goto Error; }
+
+		// 12桁分の組合せを計算する
+		for (int xor8idx = 0; xor8idx < (256 * 256 * 256); ++xor8idx) {
+			// xor8idx番目の組合せについて次の4桁を計算する
+			calcxor8 << < 256 * 256, 256 >> > (dev_xor_result12, dev_xor_result8, xor8idx);
+			cudaStatus = cudaDeviceSynchronize();		if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus); goto Error; }
+
+			// 13桁目の導出と14桁目の有効な文字コードか判定
+			calcxor13_validchk << < 256 * 256 * 256, 64 >> > (dev_31F9_is07h, dev_xor_result12, dev_chrcode_mask);
+			cudaStatus = cudaDeviceSynchronize();		if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus); goto Error; }
+
+
+			cudaStatus = cudaMemcpy(cpu_31F9_valid, dev_31F9_is07h, 64 * 64 * 64 * 64 * 64 * sizeof(unsigned char), cudaMemcpyDeviceToHost);
+			for (int chkrs = 0; chkrs < (256 * 256 * 256 * 64); ++chkrs) {
+				validcnt += cpu_31F9_valid[chkrs];
+			}
+
+			printf("%d items\n", validcnt);
+			validcnt = 0;
+		}
+	}
+//	kerntest <<< dim3(256*256), dim3(256) >>> ();
+
+	// Check for any errors launching the kernel
+	cudaStatus = cudaGetLastError();			if (cudaStatus != cudaSuccess) { fprintf(stderr, "checkPassKernel launch failed: %s\n", cudaGetErrorString(cudaStatus)); goto Error; }
+
+	// cudaDeviceSynchronize waits for the kernel to finish, and returns
+// any errors encountered during the launch.
+	cudaStatus = cudaDeviceSynchronize();		if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus); goto Error; }
+
+	// Copy output vector from GPU buffer to host memory.
+	cudaStatus = cudaMemcpy(cpu_result, dev_xor_result, 64 * 64 * 64 * 64 * sizeof(unsigned char), cudaMemcpyDeviceToHost);
+	if (cudaStatus != cudaSuccess) { fprintf(stderr, "cudaMemcpy failed!"); goto Error; }
+
+
+Error:
+	cudaFree(cpu_31F9_valid);
+	cudaFree(dev_31F9_is07h);
+	cudaFree(dev_xor_result12);
+	cudaFree(dev_xor_result8);
+	cudaFree(dev_xor_result);
+	cudaFree(dev_chrcode_mask);
+
+	return(cudaStatus);
+
+}
+#if 0
 // デバイスとのやり取り
 cudaError_t chkPass(
 	unsigned char *result_holder,		// OUTPUT 計算結果の受取先
@@ -361,85 +537,4 @@ Error:
 	return(cudaStatus);
 }
 
-#if 0
-// Helper function for using CUDA to add vectors in parallel.
-cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size)
-{
-    int *dev_a = 0;
-    int *dev_b = 0;
-    int *dev_c = 0;
-    cudaError_t cudaStatus;
-
-    // Choose which GPU to run on, change this on a multi-GPU system.
-    cudaStatus = cudaSetDevice(0);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaSetDevice failed!  Do you have a CUDA-capable GPU installed?");
-        goto Error;
-    }
-
-    // Allocate GPU buffers for three vectors (two input, one output)    .
-    cudaStatus = cudaMalloc((void**)&dev_c, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_a, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMalloc((void**)&dev_b, size * sizeof(int));
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMalloc failed!");
-        goto Error;
-    }
-
-    // Copy input vectors from host memory to GPU buffers.
-    cudaStatus = cudaMemcpy(dev_a, a, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    cudaStatus = cudaMemcpy(dev_b, b, size * sizeof(int), cudaMemcpyHostToDevice);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-    // Launch a kernel on the GPU with one thread for each element.
-    addKernel<<<1, size>>>(dev_c, dev_a, dev_b);
-
-    // Check for any errors launching the kernel
-    cudaStatus = cudaGetLastError();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addKernel launch failed: %s\n", cudaGetErrorString(cudaStatus));
-        goto Error;
-    }
-    
-    // cudaDeviceSynchronize waits for the kernel to finish, and returns
-    // any errors encountered during the launch.
-    cudaStatus = cudaDeviceSynchronize();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceSynchronize returned error code %d after launching addKernel!\n", cudaStatus);
-        goto Error;
-    }
-
-    // Copy output vector from GPU buffer to host memory.
-    cudaStatus = cudaMemcpy(c, dev_c, size * sizeof(int), cudaMemcpyDeviceToHost);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaMemcpy failed!");
-        goto Error;
-    }
-
-Error:
-    cudaFree(dev_c);
-    cudaFree(dev_a);
-    cudaFree(dev_b);
-    
-    return cudaStatus;
-}
 #endif
-
